0e472d31c6c475a80a8265d43bfc0de8b06ec0f3
Michael Spellacy (Spell) <spell@spellacy.net>
2023-10-11 15:45:54 -0400

    Misc

--- load-more/init.js
+++ load-more/init.js
@@ -3,7 +3,7 @@
   Radancy Component Library: Load More
 
   Contributor(s):
-  Michael "Spell" Spellacy, Email: michael.spellacy@radancy.com, Twitter: @spellacy, GitHub: michaelspellacy
+  Michael "Spell" Spellacy
 
   Dependencies: jQuery
 
