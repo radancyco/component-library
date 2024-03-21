d788de6be943dda8aa2502457cfb284851766542
Michael Spellacy (Spell) <spell@spellacy.net>
2024-03-21 19:04:52 -0400

    dfdfd

--- read-more/init.js
+++ read-more/init.js
@@ -9,39 +9,101 @@
 
 */
 
-// Display which Grid version in use via console:
+// Display which Read More version in use via console:
 
-console.log("%c Read More v1.0 in use. ", "background: #6e00ee; color: #fff");
+console.log("%c Read More v1.1 in use. ", "background: #6e00ee; color: #fff");
 
-$(".read-more").append("<button class='read-more__btn' aria-expanded='false'>Read More <span class='read-more__btn--icon' aria-hidden='true'></span></button>");
+var $readMoreClass = $(".read-more");
+var $readMoreContentClass = $(".read-more__content");
+var readMoreDataBottom = "data-button-bottom";
+var readMoreDataLabel = "data-button-label";
+
+$readMoreClass.each(function( ) {
+
+  var readMoreDataBottomAttr = $(this).attr(readMoreDataBottom);
+  var readMoreDataLabelAttr = $(this).attr(readMoreDataLabel);
+
+  if (typeof readMoreDataLabelAttr !== typeof undefined && readMoreDataLabelAttr !== false) {
+
+    var label = readMoreDataLabelAttr;
+
+  } else { 
+
+    var label = "Read More";
+
+  }
+
+  var readMoreButton = "<button class='read-more__btn' aria-expanded='false'>" + label + " <span class='read-more__btn--icon' aria-hidden='true'></span></button>";
+
+  if (typeof readMoreDataBottomAttr !== typeof undefined && readMoreDataBottomAttr !== false) {
+
+    $(this).append(readMoreButton);
+
+  } else {
+
+    $(this).prepend(readMoreButton);
+
+  }
+
+});
+
+var $readMoreBtnClass = $(".read-more__btn");
 
 var $focusElms = "a, audio, button, input, select, video, *[tabindex]";
 
-$(".read-more__content").attr("aria-hidden", "true").find($focusElms).attr("tabindex", "-1");
+$readMoreContentClass.attr("aria-hidden", "true").find($focusElms).attr("tabindex", "-1");
 
-$(".read-more__btn").on("click", function() {
+$readMoreBtnClass.on("click", function() {
 
-  $(this).attr("aria-expanded", function (i, attr) {
+  toggleContent($(this));
+
+});
+
+function toggleContent(target) {
+
+  target.attr("aria-expanded", function (i, attr) {
 
     return attr == "true" ? "false" : "true"
 
   });
 
-  var $parentTarget = $(this).prev();
-  var $parentTargetAttr = $(this).prev().attr("tabindex");
+  var attr = target.parent().attr(readMoreDataBottom);
+
+  if (typeof attr !== typeof undefined && attr !== false) {
+
+    var $parentTarget = target.prev();
+    var $parentTargetAttr = target.prev().attr("tabindex");
+
+  } else {
+
+    var $parentTarget = target.next();
+    var $parentTargetAttr = target.next().attr("tabindex");
+
+  }
 
   if (typeof $parentTargetAttr !== typeof undefined && $parentTargetAttr !== false) {
 
     $parentTarget.removeAttr("tabindex").attr("aria-hidden", "true");
     $parentTarget.find($focusElms).attr("tabindex", "-1");
 
-    $("html, body").animate({scrollTop: $parentTarget.offset().top}, 0);
+    if (typeof attr !== typeof undefined && attr !== false) {
+
+      $("html, body").animate({scrollTop: $parentTarget.offset().top}, 0);
+
+    }
 
   } else {
 
-    $parentTarget.removeAttr("aria-hidden").attr("tabindex", "-1").focus();
+    $parentTarget.removeAttr("aria-hidden").attr("tabindex", "-1");
+
+    if (typeof attr !== typeof undefined && attr !== false) {
+
+      $parentTarget.focus();
+
+    }
+
     $parentTarget.find($focusElms).attr("tabindex", "0");
 
   }
 
-});
+}
