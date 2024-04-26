b94a997c6d930dffa0ea530e35a03258c57fd587
Michael Spellacy (Spell) <spell@spellacy.net>
2024-04-26 11:46:22 -0400

    uttyt

--- animation-toggle/script.js
+++ animation-toggle/script.js
@@ -214,15 +214,19 @@ loadAnimationToggle("https://services.tmpwebeng.com/component-library/language-p
 
         // Note: https://stackoverflow.com/questions/36803176/how-to-prevent-the-play-request-was-interrupted-by-a-call-to-pause-error/37172024#37172024
 
-        var isPlaying = video.currentTime > 0 && !video.paused && !video.ended && video.readyState > video.HAVE_CURRENT_DATA;
+        if(!video.parentNode.querySelector(atPauseButtonClass).hasAttribute("disabled")) {
 
-        if(!isPlaying) {
+          var isPlaying = video.currentTime > 0 && !video.paused && !video.ended && video.readyState > video.HAVE_CURRENT_DATA;
 
-          video.play();
+          if(!isPlaying) {
 
-        } else {
+            video.play();
 
-          video.pause();
+          } else {
+
+            video.pause();
+
+          }
 
         }
 
