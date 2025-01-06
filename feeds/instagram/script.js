a79af1f52746884774bd4062fc2199e5441396c6
Michael Spellacy (Spell) <spell@spellacy.net>
2025-01-06 12:37:16 -0500

    Instagram Update

--- feeds/instagram/script.js
+++ feeds/instagram/script.js
@@ -1,6 +1,6 @@
 /*!
 
-  Radancy Component Library: {{ include.title }}
+  Radancy Component Library: Instagram Feed
 
   Contributor(s):
   Bobby KC
@@ -14,10 +14,23 @@
 
   // Display which version in use via console:
 
-  console.log("%c {{ include.title }} v{{ include.version }} in use. ", "background: #6e00ee; color: #fff");
+  console.log("%c Instagram Feed v1.0 in use. ", "background: #6e00ee; color: #fff");
 
   var instagramFeed = document.createElement("script");
   instagramFeed.setAttribute("src", "https://services.tmpwebeng.com/tmp-share/widget/js/instagram.js?user=wegmans&caption=n&count=6");
   document.body.appendChild(instagramFeed);
+  
+  var instagramContainer = document.getElementById("tmp-instagram-feed");
+  
+  if (instagramContainer) {
+    
+    if (!instagramContainer.children.length) {
+      
+      instagramContainer.classList.add("no-feed-available");
+      instagramContainer.innerText = "Sorry, no feed content is available. Please reach out to Radancy for support.";
+      
+    }
+    
+  }
 
 })();
