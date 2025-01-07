16dd76c688b99da902279be323bd8a228275d8e2
Michael Spellacy (Spell) <spell@spellacy.net>
2025-01-07 13:25:11 -0500

    Disclosure Update

--- disclosure/script.js
+++ disclosure/script.js
@@ -4,7 +4,6 @@
 
   Contributor(s):
   Michael "Spell" Spellacy
-  Andrew Hill
 
 */
 
@@ -18,13 +17,17 @@
 
   // Commonly used Classes, Data Attributes, States, Strings, etc.
 
-  var disclosure = document.querySelectorAll(".disclosure");
-  var disclosureBtn = document.querySelectorAll(".disclosure--btn");
-  var disclosureContent = document.querySelectorAll(".disclosure--content");
+  var disclosureButtonClass = ".disclosure--btn";
+  var disclosureButtons = document.querySelectorAll(disclosureButtonClass);
+  var disclosureArrowClassName = "disclosure--arrow";
+  var disclosureHeadingClass = ".disclosure--heading";
+  var disclosureDataDefaultOpen = "data-open";
+  var disclosureDataEnableAnchor = "data-enable-anchor";
+  var disclosureDataRemoveArrow = "data-remove-arrow";
 
   // Add Listener and other needed attributes to disclosure button
 
-  disclosureBtn.forEach(function(button, e) {
+  disclosureButtons.forEach(function(button, e) {
 
     var count = e + 1;
 
@@ -51,11 +54,11 @@
     button.setAttribute("id", thisButtonID);
     button.setAttribute("aria-controls", thisContentID);
 
-    if(button.closest(".disclosure--heading")) {
+    if(button.closest(disclosureHeadingClass)) {
 
-      // If button contains heading.
+      // If button contains heading...
 
-      button.closest(".disclosure--heading").nextElementSibling.setAttribute("id", thisContentID);
+      button.closest(disclosureHeadingClass).nextElementSibling.setAttribute("id", thisContentID);
 
     } else {
 
@@ -63,30 +66,37 @@
 
     }
 
-    // If button needs icon
+    // If custom arrow not needed.
 
-    if(button.hasAttribute("data-disclosure-icon")){
+    if(!button.hasAttribute(disclosureDataRemoveArrow)){
 
-      button.insertAdjacentHTML("beforeend", " <span class='disclosure--icon' aria-hidden='true'></span>");
+      var toggleState = document.createElement("span");
 
-      // Note: We add icon in span with aria-hidden so that is not read back by AT. For example, we don't want to hear "Learn More Plus Sign", etc.
-      // You can roll your own custom icons if you like, say an inline SVG, but be sure to always hide it.
+      toggleState.setAttribute("aria-hidden", "true");
+      toggleState.classList.add(disclosureArrowClassName);
+      button.append(toggleState);
 
     }
 
-    button.addEventListener("click", function () {
+    button.addEventListener("click", function() {
 
-      discloseContent(this);
+      var isExpanded = button.getAttribute("aria-expanded") === "true";
+
+      button.setAttribute("aria-expanded", isExpanded ? "false" : "true");
+
+      if(button.hasAttribute(disclosureDataEnableAnchor)) {
+
+        history.pushState(null, null, "#" + button.id);
+
+      }
 
     });
 
     // Open desired disclosure.
 
-    if(button.hasAttribute("data-disclosure-open")) {
+    if(button.hasAttribute(disclosureDataDefaultOpen)) {
 
-      button.click();
-
-      // button.dispatchEvent(new Event("click")); use this in future.
+      button.setAttribute("aria-expanded", "true");
 
     }
 
@@ -100,151 +110,10 @@
 
   if(disclosureID) {
 
-    if(disclosureID.closest(".disclosure--heading")) {
+    var disclosureTarget = disclosureButtonClass + "#" + disclosureID.id;
 
-      disclosureID.closest(".disclosure--heading").classList.add("open");
-
-    } else {
-
-      disclosureID.classList.add("open");
-
-    }
-
-    if(document.querySelector(".disclosure--btn#" + disclosureID.id)) {
-
-      document.querySelector(".disclosure--btn#" + disclosureID.id).setAttribute("aria-expanded", "true");
-
-    }
-
-  }
-
-  function discloseContent(thisButton){
-
-    // Run Event
-
-    var isActive = thisButton.getAttribute("aria-expanded");
-
-    if (isActive === "true") {
-
-      // Not Active
-
-      thisButton.setAttribute("aria-expanded", "false");
-
-      if(thisButton.closest(".disclosure--heading")) {
-
-        thisButton.closest(".disclosure--heading").classList.remove("open");
-
-      } else {
-
-        thisButton.classList.remove("open");
-
-      }
-
-    } else {
-
-      // Active
-
-      thisButton.setAttribute("aria-expanded", "true");
-
-      if(thisButton.closest(".disclosure--heading")) {
-
-        thisButton.closest(".disclosure--heading").classList.add("open");
-
-      } else {
-
-        thisButton.classList.add("open");
-
-      }
-
-    }
-
-    if(thisButton.hasAttribute("data-disclosure-enable-url")) {
-
-      history.pushState(null, null, "#" + thisButton.id);
-
-    }
-
-    // Pass content to div
-
-    if(thisButton.hasAttribute("data-disclosure-dynamic")){
-
-      var dynamicTarget = thisButton.closest(".disclosure-dynamic").querySelector(".disclosure--target");
-
-      dynamicTarget.innerHTML = thisButton.nextElementSibling.innerHTML;
-
-      // We only want focus and states to run on click, not initial page load.
-
-      if (document.readyState === "complete") {
-
-        // Set focus to dynamic content
-
-        dynamicTarget.setAttribute("tabindex", "-1");
-        dynamicTarget.focus();
-
-        // Basic button toggle.
-        // TODO: This seems redundant. Move to global function
-
-        var allButtons = thisButton.closest(".disclosure-dynamic").querySelectorAll("[data-disclosure-dynamic]");
-
-        allButtons.forEach(function(button, e) {
-
-          button.setAttribute("aria-expanded", "false");
-
-          if(button.closest(".disclosure--heading")) {
-
-            button.closest(".disclosure--heading").classList.remove("open");
-
-          } else {
-
-            button.classList.remove("open");
-
-          }
-
-          thisButton.setAttribute("aria-expanded", "true");
-
-          if(thisButton.closest(".disclosure--heading")) {
-
-            thisButton.closest(".disclosure--heading").classList.add("open");
-
-          } else {
-
-            thisButton.classList.add("open");
-
-          }
-
-        });
-
-      }
-
-    }
-
-    // Check if all buttons are open
-
-    if(thisButton.closest(".disclosure")) {
-
-      var buttonArray = [];
-
-      var allGroupButtons = thisButton.closest(".disclosure").querySelectorAll(".disclosure--btn");
-
-      allGroupButtons.forEach(function(button, e) {
-
-        buttonArray.push(button.getAttribute("aria-expanded"));
-
-      });
-
-      // console.log(buttonArray);
-
-      if(buttonArray.includes("false")) {
-
-        thisButton.closest(".disclosure").querySelector(".disclosure--toggle-all").setAttribute("aria-pressed", "false");
-
-      } else {
-
-        thisButton.closest(".disclosure").querySelector(".disclosure--toggle-all").setAttribute("aria-pressed", "true");
-
-      }
-
-    }
+    document.querySelector(disclosureTarget).setAttribute("aria-expanded", "true");
+    document.querySelector(disclosureTarget).focus();
 
   }
 
