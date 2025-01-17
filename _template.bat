980b682f44906b5c75db199c5b20a4195a69d36e
Michael Spellacy (Spell) <spell@spellacy.net>
2025-01-17 15:04:49 -0500

    misc

--- _template.bat
+++ _template.bat
@@ -8,25 +8,3 @@ xcopy /y _layouts\default.html ..\playground\_layouts\
 xcopy /y _layouts\default.html ..\radancyco.github.io\_layouts\
 xcopy /y _layouts\default.html ..\tb-ajd-template\_layouts\
 xcopy /y _layouts\default.html ..\tmp-custom-imports\_layouts\
-
-REM Primary Template
-
-xcopy /y _includes\template.html ..\a11y\_includes\
-xcopy /y _includes\template.html ..\career-path-generator\_includes\
-xcopy /y _includes\template.html ..\component-library\_includes\
-xcopy /y _includes\template.html ..\magicbullet\_includes\
-xcopy /y _includes\template.html ..\playground\_includes\
-xcopy /y _includes\template.html ..\radancyco.github.io\_includes\
-xcopy /y _includes\template.html ..\tb-ajd-template\_includes\
-xcopy /y _includes\template.html ..\tmp-custom-imports\_includes\
-
-REM Primary Navigation
-
-xcopy /y _includes\navigation.html ..\a11y\_includes\
-xcopy /y _includes\navigation.html ..\career-path-generator\_includes\
-xcopy /y _includes\navigation.html ..\component-library\_includes\
-xcopy /y _includes\navigation.html ..\magicbullet\_includes\
-xcopy /y _includes\navigation.html ..\playground\_includes\
-xcopy /y _includes\navigation.html ..\radancyco.github.io\_includes\
-xcopy /y _includes\navigation.html ..\tb-ajd-template\_includes\
-xcopy /y _includes\navigation.html ..\tmp-custom-imports\_includes\
